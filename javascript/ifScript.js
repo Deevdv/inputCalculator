46dd93d8d18b77d16a03d454595be0622e12057c
Deevdv <110405472+Deevdv@users.noreply.github.com>
2023-01-27 02:36:07 +0200

    3 input calc
    rev 3

--- javascript/ifScript.js
+++ javascript/ifScript.js
@@ -1,4 +1,4 @@
-// take the operand input
+// take the number input
 const number1 = parseFloat(prompt('Enter first number: '));
 const number2 = parseFloat(prompt('Enter second number: '));
 // take the operator input
